77f6ae2375a4e56e6da82a2dc61f1c6032f3151b
unknown <natanflisboa1@gmail.com>
2021-05-27 02:34:21 -0300

    Renomeia pacotes

---
+++ src/semana3threads/thread/aula/MinhaThread.java
@@ -0,0 +1,30 @@
+package semana3threads.thread.aula;
+
+// Cria uma thread estendendo a classe "Thread"
+public class MinhaThread extends Thread {
+
+	private String nome;
+	private int tempo;
+	
+	public MinhaThread(String nome, int tempo) {
+		this.nome = nome;
+		this.tempo = tempo;
+		start();
+	}
+
+	public void run() {
+		
+		try {
+			for (int i=0; i<6; i++) {
+				System.out.println(nome + " contador " + i);
+				Thread.sleep(tempo);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(nome + " terminou a execução");
+		
+	}
+}
+
