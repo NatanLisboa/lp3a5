be28d7b8f9f6a4373c2c5809d59d7723331aa51b
unknown <natanflisboa1@gmail.com>
2021-06-05 14:33:23 -0300

    Adiciona wait, notify, notifyAll

---
+++ src/semana5semaforos/aulas/aulawaitnotifynotifyall/TiqueTaque.java
@@ -0,0 +1,55 @@
+package semana5semaforos.aulas.aulawaitnotifynotifyall;
+
+public class TiqueTaque {
+
+	boolean tique;
+
+	synchronized void tique(boolean estaExecutando) {
+
+		if (!estaExecutando) {
+			tique = true;
+			notify();
+			return;
+		}
+
+		System.out.print("Tique ");
+
+		tique = true;
+
+		notify();
+		
+		try {
+			while (tique) {
+				wait();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	synchronized void taque(boolean estaExecutando) {
+
+		if (!estaExecutando) {
+			tique = false;
+			notify();
+			return;
+		}
+
+		System.out.println("Taque");
+
+		tique = false;
+
+		notify();
+
+		while (!tique) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
